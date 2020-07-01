9aba7baf458443b1abdc83876fedd1ed43cfafd7
Daniel Pritchett <daniel.pritchett@crowdstrike.com>
2020-07-01 11:55:19 -0500

    Corrected git-checkout-nomasters download URL

--- git-checkout-nomasters.rb
+++ git-checkout-nomasters.rb
@@ -2,7 +2,7 @@ class GitCheckoutNomasters < Formula
   desc "Train yourself to stop using a `master` branch"
   homepage "https://github.com/shelltoys/git-helpers"
 
-  url "https://github.com/shelltoys/git-helpers/blob/da1639d17437f96395e57f545a64b3caacb17f89/git-checkout-nomasters"
+  url "https://raw.githubusercontent.com/shelltoys/git-helpers/da1639d17437f96395e57f545a64b3caacb17f89/git-checkout-nomasters"
   version "0.0.1"
   sha256 "9d8aa815340181e09da60669ace41b9d974636339f7a36294baca26f32624aa2"
 
