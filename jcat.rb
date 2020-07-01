93e404588d3b747c93e0dd120b39c1250936ac0e
Daniel Pritchett <daniel.pritchett@crowdstrike.com>
2020-07-01 11:08:41 -0500

    Hardlinked jcat source to github

--- jcat.rb
+++ jcat.rb
@@ -1,7 +1,7 @@
 class Jcat < Formula
   desc "JQuery cat wrapper around jq"
   homepage "https://github.com/shelltoys/cats"
-  url "https://github.com/shelltoys/cats"
+  url "https://raw.githubusercontent.com/shelltoys/cats/aa73a73164719eeb69492815593d184163679772/jcat"
   version "0.0.1"
   sha256 "ed544bab01cb2720f8f0b831db8f26b25f8b30ec3ee5f7694a7ef928e6dbdc9c"
 
