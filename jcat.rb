6485fffa5392c2bd604765d80f5f1bc1f6c473e9
Daniel Pritchett <daniel.pritchett@crowdstrike.com>
2020-07-01 10:29:56 -0500

    Tweak jcat with url field

--- jcat.rb
+++ jcat.rb
@@ -1,7 +1,9 @@
 class Jcat < Formula
   desc "jquery cat wrapper around jq"
   homepage "https://github.com/shelltoys/cats"
-  sha256 "3cfbeee701f61aef1c8548a66c1f04a4d417b4d9"
+  url "https://github.com/shelltoys/cats/tree/v0.0.1"
+  version "0.0.1"
+  sha256 "aa73a73164719eeb69492815593d184163679772"
 
   def install
     bin.install 'jcat'
