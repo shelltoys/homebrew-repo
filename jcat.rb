47b759adecde79b2d61a4bd9ee41cc27897f7d11
Daniel Pritchett <daniel.pritchett@crowdstrike.com>
2020-07-01 10:27:24 -0500

    Adds sha256 for jcat

--- jcat.rb
+++ jcat.rb
@@ -1,7 +1,7 @@
 class Jcat < Formula
   desc "jquery cat wrapper around jq"
   homepage "https://github.com/shelltoys/cats"
-  head "https://github.com/shelltoys/cats.git"
+  sha256 "3cfbeee701f61aef1c8548a66c1f04a4d417b4d9"
 
   def install
     bin.install 'jcat'
